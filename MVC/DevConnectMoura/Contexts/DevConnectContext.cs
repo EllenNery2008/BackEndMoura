583a4101e69b701b2d54a74dde4ad2ed0416ee61
Ellen <ellennery9@gmail.com>
2025-12-12 14:14:49 -0300

    Fase de projeto - DevConnect

---
+++ MVC/DevConnectMoura/Contexts/DevConnectContext.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using DevConnectMoura.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevConnectMoura.Contexts;
+
+public partial class DevConnectContext : DbContext
+{
+    public DevConnectContext()
+    {
+    }
+
+    public DevConnectContext(DbContextOptions<DevConnectContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<TbComentario> TbComentario { get; set; }
+
+    public virtual DbSet<TbCurtida> TbCurtida { get; set; }
+
+    public virtual DbSet<TbPublicacao> TbPublicacao { get; set; }
+
+    public virtual DbSet<TbUsuario> TbUsuario { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=DevCon_SA");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TbComentario>(entity =>
+        {
+            entity.HasKey(e => e.IdComentario).HasName("PK__tb_comen__1BA6C6F402B6CBB4");
+
+            entity.HasOne(d => d.IdPublicacaoNavigation).WithMany(p => p.TbComentario)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__tb_coment__id_pu__5535A963");
+
+            entity.HasOne(d => d.IdUsuarioNavigation).WithMany(p => p.TbComentario)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__tb_coment__id_us__5441852A");
+        });
+
+        modelBuilder.Entity<TbCurtida>(entity =>
+        {
+            entity.HasKey(e => e.IdCurtida).HasName("PK__tb_curti__0AC033CA8C30C456");
+
+            entity.HasOne(d => d.IdPublicacaoNavigation).WithMany(p => p.TbCurtida)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__tb_curtid__id_pu__5165187F");
+
+            entity.HasOne(d => d.IdUsuarioNavigation).WithMany(p => p.TbCurtida)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__tb_curtid__id_us__5070F446");
+        });
+
+        modelBuilder.Entity<TbPublicacao>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__tb_publi__3213E83FA1672799");
+
+            entity.HasOne(d => d.IdUsuarioNavigation).WithMany(p => p.TbPublicacao).HasConstraintName("FK__tb_public__id_us__4D94879B");
+        });
+
+        modelBuilder.Entity<TbUsuario>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__tb_usuar__3213E83FFA5CE991");
+
+            entity.HasMany(d => d.IdSeguindo).WithMany(p => p.IdUsuarioSeguidor)
+                .UsingEntity<Dictionary<string, object>>(
+                    "TbSeguidor",
+                    r => r.HasOne<TbUsuario>().WithMany()
+                        .HasForeignKey("IdSeguindo")
+                        .OnDelete(DeleteBehavior.ClientSetNull)
+                        .HasConstraintName("FK__tb_seguid__id_se__59063A47"),
+                    l => l.HasOne<TbUsuario>().WithMany()
+                        .HasForeignKey("IdUsuarioSeguidor")
+                        .OnDelete(DeleteBehavior.ClientSetNull)
+                        .HasConstraintName("FK__tb_seguid__id_us__5812160E"),
+                    j =>
+                    {
+                        j.HasKey("IdUsuarioSeguidor", "IdSeguindo").HasName("PK__tb_segui__44E9DBEDF03965E9");
+                        j.ToTable("tb_seguidor");
+                        j.IndexerProperty<int>("IdUsuarioSeguidor").HasColumnName("id_usuario_seguidor");
+                        j.IndexerProperty<int>("IdSeguindo").HasColumnName("id_seguindo");
+                    });
+
+            entity.HasMany(d => d.IdUsuarioSeguidor).WithMany(p => p.IdSeguindo)
+                .UsingEntity<Dictionary<string, object>>(
+                    "TbSeguidor",
+                    r => r.HasOne<TbUsuario>().WithMany()
+                        .HasForeignKey("IdUsuarioSeguidor")
+                        .OnDelete(DeleteBehavior.ClientSetNull)
+                        .HasConstraintName("FK__tb_seguid__id_us__5812160E"),
+                    l => l.HasOne<TbUsuario>().WithMany()
+                        .HasForeignKey("IdSeguindo")
+                        .OnDelete(DeleteBehavior.ClientSetNull)
+                        .HasConstraintName("FK__tb_seguid__id_se__59063A47"),
+                    j =>
+                    {
+                        j.HasKey("IdUsuarioSeguidor", "IdSeguindo").HasName("PK__tb_segui__44E9DBEDF03965E9");
+                        j.ToTable("tb_seguidor");
+                        j.IndexerProperty<int>("IdUsuarioSeguidor").HasColumnName("id_usuario_seguidor");
+                        j.IndexerProperty<int>("IdSeguindo").HasColumnName("id_seguindo");
+                    });
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ MVC/DevConnectMoura/Controllers/UsuarioController.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using DevConnectMoura.Contexts;
+using DevConnectMoura.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DevConnectMoura.Controllers
+{
+
+    public class UsuarioController : Controller
+    {
+        private readonly DevConnectContext _context;
+        private readonly ILogger<UsuarioController> _logger;
+
+        public UsuarioController(ILogger<UsuarioController> logger, DevConnectContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        // Tela Cadastro do usuario
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.UsuarioNovoCadastrado = "";
+            TempData["UsuarioNovoCadastrado"] = "";
+            return View();
+        }
+
+        //Tela Perfil do usuario
+        [HttpPost]
+        public async Task<IActionResult> Index(IFormCollection form)
+        {
+            TbUsuario novoUsuario = new TbUsuario()
+            {
+                NomeCompleto = form["NomeCompleto"].ToString(),
+                NomeUsuario = form["NomeUsuario"].ToString(),
+                Email = form["Email"].ToString(),
+                Senha = form["Senha"].ToString()
+            };
+
+            if (form.Files.Count > 0)
+            {
+                // Se selecionou uma imagem entra dentro do if 
+                IFormFile file = form.Files[0];
+                var folder = Path.Combine(Directory.GetCurrentDirectory(),"wwwroot/images");
+
+                if (!Directory.Exists(folder))
+                {
+                    Directory.CreateDirectory(folder);
+                }
+
+                //caminho da imagem que sera salvo
+                string path = Path.Combine(folder, file.FileName);
+
+                //salva a imagem na pasta wwwroot/images
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+
+                //passa o nome da imagem
+                novoUsuario.FotoPerfilUrl = file.FileName;
+            }
+            else
+            {
+                //ponhe a foto padrao
+                novoUsuario.FotoPerfilUrl = "";
+            }
+
+            try
+            {
+                _context.TbUsuario.Add(novoUsuario);
+
+                await _context.SaveChangesAsync();
+
+                TempData["UsuarioNovoCadastrado"] = "Cadastrado";
+                ViewBag.UsuarioNovoCadastrado = "";
+
+                return RedirectToAction("Index", "Home");
+            }
+            catch (System.Exception)
+            {
+               ViewBag.UsuarioNovoCadastrado = "Nao cadastrado";
+               TempData["UsuarioNovoCadastrado"] = "";
+               return View();
+            }
+
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
